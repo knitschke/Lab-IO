c0c1ac54526fd219fdd4ce0ab136cd18889ca409
knitschke <krzychunitschke@onet.pl>
2018-01-26 23:11:32 +0100

    Add files via upload

---
+++ 15cMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace _15c
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Serv sv = new Serv();
+            sv.Run();
+            Klient a1 = new Klient();
+            Klient a2 = new Klient();
+            Klient a3 = new Klient();
+
+            a1.Connect();
+            a2.Connect();
+            a3.Connect();
+
+            CancellationTokenSource tok1 = new CancellationTokenSource();
+            CancellationTokenSource tok2 = new CancellationTokenSource();
+            CancellationTokenSource tok3 = new CancellationTokenSource();
+
+            var client1 = a1.keepPinging("message", tok1.Token);
+            var client2 = a2.keepPinging("message", tok2.Token);
+            var client3 = a3.keepPinging("message", tok3.Token);
+
+            tok1.CancelAfter(2000);
+            tok2.CancelAfter(3000);
+            tok3.CancelAfter(4000);
+
+            Task.WaitAll(new Task[] { client1, client2, client3 });
+            sv.StopRunning();
+
+            Console.WriteLine(client1.Result);
+            Console.WriteLine(client2.Result);
+            Console.WriteLine(client3.Result);
+
+
+
+
+        }
+    }
+}
---
+++ Serv.cs
@@ -0,0 +1,125 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace _15c
+{
+class Serv
+{
+    
+    TcpListener server;
+    int port;
+    IPAddress address;
+    bool running = false;
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Task serverTask;
+
+    public Task ServerTask
+    {
+        get { return serverTask; }
+    }
+    
+
+
+
+    
+    public IPAddress Address
+    {
+        get { return address; }
+        set
+        {
+            if (!running) address = value;
+            else ;
+        }
+    }
+    public int Port
+    {
+        get { return port; }
+        set
+        {
+            if (!running)
+                port = value;
+            else ;
+        }
+    }
+    
+    
+    public Serv()
+    {
+        Address = IPAddress.Any;
+        port = 2048;
+    }
+    public Serv(int port)
+    {
+        this.port = port;
+    }
+    public Serv(IPAddress address)
+    {
+        this.address = address;
+    }
+    
+    
+
+    public async Task RunAsync(CancellationToken ct)
+    {
+
+        server = new TcpListener(address, port);
+
+        try
+        {
+            server.Start();
+            running = true;
+        }
+        catch (SocketException ex)
+        {
+            throw (ex);
+        }
+        while (true && !ct.IsCancellationRequested)
+        {
+
+            TcpClient client = await server.AcceptTcpClientAsync();
+            byte[] buffer = new byte[1024];
+            using (ct.Register(() => client.GetStream().Close()))
+            {
+                client.GetStream().ReadAsync(buffer, 0, buffer.Length, ct).ContinueWith(
+                    async (t) =>
+                    {
+                        int i = t.Result;
+                        while (true)
+                        {
+                            client.GetStream().WriteAsync(buffer, 0, i, ct);
+                            try
+                            {
+                                i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length, ct);
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                        }
+                    });
+            }
+        }
+
+    }
+    public void RequestCancellation()
+    {
+        cts.Cancel();
+
+        server.Stop();
+    }
+    public void Run()
+    {
+
+        serverTask = RunAsync(cts.Token);
+    }
+    public void StopRunning()
+    {
+        RequestCancellation();
+ 
+    }
+    
+}
+}
